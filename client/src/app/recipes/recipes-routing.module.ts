7f111386a89cb5078c92f59400388f7512f28e44
Zelda Mazur <zelda@zeldamazur.com>
2023-10-20 22:35:44 -0500

    removed container component

--- client/src/app/recipes/recipes-routing.module.ts
+++ client/src/app/recipes/recipes-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { ViewRecipeComponent } from './view-recipe/view-recipe.component';
-import { SingleRecipeContainerComponent } from './single-recipe-container/single-recipe-container.component';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 
 const routes: Routes = [
@@ -11,12 +10,12 @@ const routes: Routes = [
     component: RecipeListComponent
   },
   {
-    path: ':recipe-id',
-    component: SingleRecipeContainerComponent,
-    children: [
-      { path: 'details', component: ViewRecipeComponent },
-      { path: 'edit', component: AddRecipeComponent }
-    ]
+    path: ':recipe-id/details',
+    component: ViewRecipeComponent,
+  },
+  { 
+    path: ':recipe-id/edit', 
+    component: AddRecipeComponent 
   },
   {
     path: 'new-recipe',
